943145f07b1e3346c5c845290e9c99b826d67df0
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-02 21:32:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,3 +84,46 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
     
 end
+
+def changing_alan
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+     
+end
+
+def adding_to_dennis
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+end
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
